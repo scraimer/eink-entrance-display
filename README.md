bfb3c3a3aacb29d0a59a250a7c3d0e0d598d332b
Shalom Craimer <scraimer@gmail.com>
2021-03-25 22:17:22 +0000

    Testing weather font and pre-built PNG files

--- README.md
+++ README.md
@@ -12,6 +12,28 @@ Show information of interest to the whole family on an eInk display near the ent
 
      ./run.sh
 
+# Plan for display
+
+Have different display formats:
+
+- Chol
+- Erev Shabbat / Erev Yom Tov
+- Shabbat (switch as soon as shabbat comes in, but still show shabbat time)
+
+I want to see:
+
+- Weather forcast (can I put hourly? Or summarize interesting events during the day? Like when it might rain, or when it will be sunny, and the temperatures)
+- Zmanim: Shabbat/Yom Tov entrance, exit time, shul times (shachrit(s))
+- Next events from a Google Calendar that me and Miriam Malka can edit (e.g. next zoom lesson, etc)
+- Maybe a tiny clock with the time of the last update
+
+Data collection:
+
+Collect data with one process, that updates each segment at a relevant frequency (zmanim, update once a day. Weather, 4 times a day. Calendar, every 15 minutes)
+
+Another process should read the data, build the bitmaps (red and black) and throw them on the screen. Once every 5 minutes. (Have this run by cron, instead of relying on the process - unless tests show that keeping the process up reduces refresh time.)
+
+
 # Notes:
 
 To get text directions control, you need to install `libraqm`:
@@ -22,3 +44,5 @@ To get text directions control, you need to install `libraqm`:
      python3 -m pip install --upgrade Pillow
 
      TODO: This still doesn't work
+
+TODO: Move all this into a Docker image using the Dockerfile, for easy setup
--- display.py
+++ display.py
@@ -10,45 +10,57 @@ if os.path.exists(libdir):
 import logging
 from waveshare_epd import epd7in5b_HD
 import time
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image,ImageDraw,ImageFont, ImageFilter
 import traceback
 from urllib.parse import unquote 
 
 logging.basicConfig(level=logging.DEBUG)
 
-def create_image():
+def create_erev_shabbat_image(out_width, out_height):
     font24 = ImageFont.truetype(os.path.join(picdir, 'arial.ttf'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'arial.ttf'), 18)
+    weather_font = ImageFont.truetype(os.path.join(picdir, 'Pe-icon-7-weather.ttf'),128)
     
-    image_black = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    image_red = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    # Note: Image size is 528 width, and 880 height
+
+    image_black = Image.new('1', (out_height, out_width), 255)  # 255: clear the frame
+    image_red = Image.new('1', (out_height, out_width), 255)  # 255: clear the frame
     draw_black = ImageDraw.Draw(image_black)
     draw_red = ImageDraw.Draw(image_red)
-    draw_black.text((2, 0), 'hello world', font = font18, fill = 0)
-    draw_black.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-    hebrew_text = unquote("%D7%A9%D7%91%D7%AA%20%D7%A9%D7%9C%D7%95%D7%9D%21")
-    #draw_red.text((20, 50), hebrew_text, font = font18, fill = 0, direction = "rtl")
-    draw_red.text((20, 50), hebrew_text, font = font18, fill = 0)
-    draw_red.line((10, 90, 60, 140), fill = 0)
-    draw_red.line((60, 90, 10, 140), fill = 0)
-    draw_red.rectangle((10, 90, 60, 140), outline = 0)
-    draw_red.line((95, 90, 95, 140), fill = 0)
-    draw_black.line((70, 115, 120, 115), fill = 0)
-    draw_black.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw_black.rectangle((10, 150, 60, 200), fill = 0)
-    draw_black.chord((70, 150, 120, 200), 0, 360, fill = 0)
     
+    #draw_black.text((2, 0), 'hello world', font = font18, fill = 0)
+    #draw_black.text((2, 20), '7.5inch epd', font = font18, fill = 0)
+    
+    weather_sunny = unquote("%EE%98%8C%0A")
+    draw_red.text((2, 0), weather_sunny, font = weather_font)
+    
+    #hebrew_text = unquote("%D7%A9%D7%91%D7%AA%20%D7%A9%D7%9C%D7%95%D7%9D%21")
+    #draw_red.text((20, 50), hebrew_text, font = font18, fill = 0)
+    #draw_red.text((20, 50), hebrew_text, font = font18, fill = 0, direction = "rtl")
+    
+    #draw_red.line((10, 90, 60, 140), fill = 0)
+    #draw_red.line((60, 90, 10, 140), fill = 0)
+    #draw_red.rectangle((10, 90, 60, 500), outline = 0)
+    #draw_red.line((95, 90, 95, 140), fill = 0)
+    #draw_black.line((70, 115, 120, 115), fill = 0)
+    #draw_black.arc((70, 90, 120, 140), 0, 360, fill = 0)
+    #draw_black.rectangle((10, 150, 60, 200), fill = 0)
+    #draw_black.chord((70, 150, 120, 200), 0, 360, fill = 0)
+    
+    with Image.open(os.path.join(picdir,"black-white-landscape-5.jpg")) as im:
+        image_black.paste(im)
+
     image_black.save("black.png")
     image_red.save("red.png")
     
     return (image_black, image_red)
 
 try:
-    logging.info("epd7in5b_HD Demo")
-
-    epd = epd7in5b_HD.EPD()
+    image_black, image_red = create_erev_shabbat_image(880, 528);
+    #sys.exit(1)
     
-    image_black, image_red = create_image();
+    epd = epd7in5b_HD.EPD()
+    # TODO: assert that epd.height == 880 and epd.width == 528
 
     logging.info("init and Clear")
     epd.init()
