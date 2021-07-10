782668043adeb76e5b4b9b0515fd687c633636a1
Shalom Craimer <scraimer@gmail.com>
2021-07-10 21:33:56 +0100

    Add sneaker as red and black images

--- layout.py
+++ layout.py
@@ -5,9 +5,9 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageColor, ImageMath,
 from urllib.parse import unquote, quote
 from types import SimpleNamespace
 import scrape
+from pathlib import Path
 
-
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+picdir = Path(os.path.dirname(os.path.realpath(__file__))) / 'pic'
 
 class Shabbat:
     def __init__(self):
@@ -22,10 +22,16 @@ class Shabbat:
 def reverse(source:str):
     return source[::-1]
 
+def paste_red_and_black_image(name:str, red_image:Image, black_image:Image, position):
+    with Image.open(picdir / f"{name}-red.png") as im:
+        red_image.paste(im, position)
+    with Image.open(picdir / f"{name}-black.png") as im:
+        black_image.paste(im, position)
+
 def create_erev_shabbat_image(width:int, height:int, data:Shabbat):
-    font_title = ImageFont.truetype(os.path.join(picdir, 'arial.ttf'), 54)
-    font_text = ImageFont.truetype(os.path.join(picdir, 'arial.ttf'), 40)
-    weather_font = ImageFont.truetype(os.path.join(picdir, 'Pe-icon-7-weather.ttf'),128)
+    font_title = ImageFont.truetype(str(picdir / 'arial.ttf'), 54)
+    font_text = ImageFont.truetype(str(picdir / 'arial.ttf'), 40)
+    weather_font = ImageFont.truetype(str(picdir / 'Pe-icon-7-weather.ttf'),128)
     
     # create a single color image with black and red and white
 
@@ -34,7 +40,7 @@ def create_erev_shabbat_image(width:int, height:int, data:Shabbat):
     black_image = Image.new('1', (width, height), 255)  # 255: clear the frame
     black_draw = ImageDraw.Draw(black_image)
     
-    with Image.open(os.path.join(picdir,"black-white-landscape-5.jpg")) as im:
+    with Image.open(picdir / "black-white-landscape-5.jpg") as im:
         black_image.paste(im, (0,int(height/2)))
     
     weather_sunny_icon = unquote("%EE%98%8C%0A")
@@ -45,14 +51,17 @@ def create_erev_shabbat_image(width:int, height:int, data:Shabbat):
     
     lines = []
     shabbat_items = scrape.scrape_shabbat_items()
-    lines.append({"font":font_title, "text":reverse(shabbat_items['parasha_name'])})
+    lines.append({"font":font_title, "text":reverse(shabbat_items['parasha_name']), "center": True})
     for title,times in shabbat_items['times'].items():
         times = [t if t[-3]==':' else reverse(t) for t in times]
         lines.append({"font":font_text, "text": f"{', '.join(sorted(times, reverse=True))} :{reverse(title)}"})
     
     for line in lines:
         box = line["font"].getbbox(line["text"])
-        actual_x = width - x - box[2]
+        if line.get("center", False):
+            actual_x = (width - box[2]) / 2 - x
+        else:
+            actual_x = width - x - box[2]
         red_draw.text((actual_x, y), line["text"], font = line["font"], fill=0)
         y = y + box[3]
     
@@ -64,6 +73,8 @@ def create_erev_shabbat_image(width:int, height:int, data:Shabbat):
     #draw_black.arc((70, 90, 120, 140), 0, 360, fill = 0)
     #draw_black.rectangle((10, 150, 60, 200), fill = 0)
     #draw_black.chord((70, 150, 120, 200), 0, 360, fill = 0)
+    
+    paste_red_and_black_image(name="sneaker", red_image=red_image, black_image=black_image, position=(30,height - 50))
     
     return SimpleNamespace(red=red_image, black=black_image)
 
