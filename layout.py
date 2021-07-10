b3f61348f68284625388630e902d511972e5147e
Shalom Craimer <scraimer@gmail.com>
2021-07-10 19:45:03 +0100

    Sort the times. Remove dead code

--- layout.py
+++ layout.py
@@ -37,35 +37,24 @@ def create_erev_shabbat_image(width:int, height:int, data:Shabbat):
     with Image.open(os.path.join(picdir,"black-white-landscape-5.jpg")) as im:
         black_image.paste(im, (0,int(height/2)))
     
-    #draw_black.text((2, 0), 'hello world', font = font_text, fill = 0)
-    #draw_black.text((2, 20), '7.5inch epd', font = font_text, fill = 0)
-    
     weather_sunny_icon = unquote("%EE%98%8C%0A")
     red_draw.text((432, 700), weather_sunny_icon, font = weather_font, fill=0)
     
-    y = 50
-    x = 20
+    y = -15
+    x = 0
     
-    lines = [
-        #{"font": font_title, "text": reverse(unquote("%D7%A4%D7%A8%D7%A9%D7%AA") + " " + unquote(data.parasha_name))},
-        #{"font": font_text,  "text": data.early_shabbat + " " + reverse(unquote("%D7%A7%D7%91%D7%9C%D7%AA %D7%A9%D7%91%D7%AA %D7%9E%D7%95%D7%A7%D7%93%D7%9E%D7%AA:"))},
-        #{"font": font_text,  "text": data.candle_lighting + " " + reverse(unquote("%D7%94%D7%93%D7%9C%D7%A7%D7%AA%20%D7%A0%D7%A8%D7%95%D7%AA"))},
-        #{"font": font_text,  "text": data.shabbat_end + " " + reverse(unquote("%D7%A2%D7%A8%D7%91%D7%99%D7%AA:"))},
-    ]
-
+    lines = []
     shabbat_items = scrape.scrape_shabbat_items()
     lines.append({"font":font_title, "text":reverse(shabbat_items['parasha_name'])})
     for title,times in shabbat_items['times'].items():
         times = [t if t[-3]==':' else reverse(t) for t in times]
-        lines.append({"font":font_text, "text": f"{', '.join(times)} :{reverse(title)}"})
+        lines.append({"font":font_text, "text": f"{', '.join(sorted(times, reverse=True))} :{reverse(title)}"})
     
     for line in lines:
         box = line["font"].getbbox(line["text"])
         actual_x = width - x - box[2]
         red_draw.text((actual_x, y), line["text"], font = line["font"], fill=0)
         y = y + box[3]
-
-    #draw_red.text((20, 50), hebrew_text, font = font_text, fill = 0, direction = "rtl")
     
     #draw_red.line((10, 90, 60, 140), fill = 0)
     #draw_red.line((60, 90, 10, 140), fill = 0)
@@ -94,46 +83,12 @@ def join_image(source_red:Image, source_black:Image):
     out = Image.merge("RGB", (out_r,out_b,out_g))
     return out
 
-def join_image_slow(source_red:Image, source_black:Image):
-    width, height = source_red.size
-    
-    color_red = ImageColor.getrgb("red")
-    color_black = ImageColor.getrgb("black")
-    color_white = ImageColor.getrgb("white")
-    out_image = Image.new('RGB', (width, height), color_white) # 255: clear the frame
-    red_data = source_red.getdata()
-    black_data = source_black.getdata()
-    out_data = list(out_image.getdata())
-    for i,px in enumerate(red_data):
-        x = i % width
-        y = int( i / width )
-        percentage = (int((y*10000)/height)*1)/100
-        if x == 0 and y % 10 == 0:
-            print(f"line: {y} [{percentage}%]")
-            
-        if red_data[i] == 0:
-            out_data[i] = color_red
-        elif black_data[i] == 0:
-            out_data[i] = color_black
-        else:
-            out_data[i] = color_white
-            
-    out_image.putdata( out_data )
-    return out_image
-
 def make_image():
     shabbat = Shabbat()
     # Note: Image size is 528 width, and 880 height
-    
     shabbat_image = create_erev_shabbat_image(width=528, height=880, data=shabbat)
-    
-    # XXX: Debug, save to file
-    shabbat_image.black.save("black.png")
-    shabbat_image.red.save("red.png")
-    
     color_image = join_image( source_black=shabbat_image.black, source_red=shabbat_image.red )
     
-    # XXX
     # XXX: Debug, save to file
     color_image.save("color.png")
     
